7bebac3f1aa231d82565ac9a368f5a9d4c5d5eac
OWILX <greatenyinna10@gmail.com>
2026-03-17 00:13:47 +0100

    Improving login page

--- app/web/assets/js/app.js
+++ app/web/assets/js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Wait for splash animation, then redirect
   setTimeout(() => {
-      //window.location.href = 'https://letstudy.infinityfreeapp.com/app/dashboard.php';
+      window.location.href = '/app/web/login.html';
   }, 2000); // Matches splash duration (1.8s animation + buffer)
 });
