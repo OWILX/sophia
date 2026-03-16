67b04e5a505a391fe95cda45829a304de13eeedb
OWILX <greatenyinna10@gmail.com>
2026-03-16 23:59:38 +0100

    Improving index page

--- app/web/assets/js/app.js
+++ app/web/assets/js/app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Wait for splash animation, then redirect
   setTimeout(() => {
-      window.location.href = 'https://letstudy.infinityfreeapp.com/app/dashboard.php';
+      //window.location.href = 'https://letstudy.infinityfreeapp.com/app/dashboard.php';
   }, 2000); // Matches splash duration (1.8s animation + buffer)
 });
