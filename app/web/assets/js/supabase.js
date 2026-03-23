4bb60187147179f65eaa81aa7c8158c60249fb4e
OWILX <greatenyinna10@gmail.com>
2026-03-23 08:10:46 +0100

    fetching courses

--- app/web/assets/js/supabase.js
+++ app/web/assets/js/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 const SUPABASE_URL = "https://nlenaoincibjuyejhmak.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_UVe9_-V8CSy0Jujg4JjcnQ_MXP6F2Ap";
 export const client = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
