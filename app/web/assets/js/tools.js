d048a48cc3bc92e6009be327e361c5e8b54391f2
OWILX <greatenyinna10@gmail.com>
2026-05-18 11:31:48 +0100

    Fixed toast

--- app/web/assets/js/tools.js
+++ app/web/assets/js/tools.js
@@ -1,16 +1,4 @@
-/**export function showToast(message, type = 'info', time_ms = 3000) {
-    const container = document.getElementById('toast-container');
-    const toast = document.createElement('div');
-    toast.className = `toast ${type}`;
-    toast.textContent = message;
-    container.appendChild(toast);
-
-    // Auto remove after animation
-    setTimeout(() => {
-        toast.remove();
-    }, time_ms);
-}*/
-export function showToast(message, type = 'info', time_ms = 3000) {
+/*export function showToast(message, type = 'info', time_ms = 3000) {
     const container = document.getElementById('toast-container');
     const toast = document.createElement('div');
     toast.className = `toast ${type}`;
@@ -27,6 +15,41 @@ export function showToast(message, type = 'info', time_ms = 3000) {
         toast.classList.add('fade-out');
 
         toast.addEventListener('transitionend', () => {
+            toast.remove();
+        });
+    }, time_ms);
+}
+*/
+let activeToast = null;
+
+export function showToast(message, type = 'info', time_ms = 3000) {
+    const container = document.getElementById('toast-container');
+
+    // Remove existing toast
+    if (activeToast) {
+        activeToast.remove();
+    }
+
+    const toast = document.createElement('div');
+    toast.className = `toast ${type}`;
+    toast.textContent = message;
+
+    container.appendChild(toast);
+    activeToast = toast;
+
+    requestAnimationFrame(() => {
+        toast.classList.add('show');
+    });
+
+    setTimeout(() => {
+        toast.classList.remove('show');
+        toast.classList.add('fade-out');
+
+        toast.addEventListener('transitionend', () => {
+            if (activeToast === toast) {
+                activeToast = null;
+            }
+
             toast.remove();
         });
     }, time_ms);
